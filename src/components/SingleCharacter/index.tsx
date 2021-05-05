fa8360ab6b5b6c855f723ddc82c496bff6819965
Gregfanyan <grigorfanyan@gmail.com>
2021-05-05 21:35:32 +0200

    feat: added back button from single Character page

--- src/components/SingleCharacter/index.tsx
+++ src/components/SingleCharacter/index.tsx
@@ -1,23 +1,31 @@
 import React from "react";
-import { useParams } from "react-router-dom";
+import { useParams, useHistory } from "react-router-dom";
 
 function SingleCharacter({ character }: any) {
   const { id } = useParams<any>();
+  const history = useHistory();
+
   const singleCharacter = character.find(
     (char: any) => char.id.toString() === id
   );
   const { image, name, status } = singleCharacter;
 
-  console.log("singleCharacter", singleCharacter);
+  function handleClick() {
+    if (!history) {
+      return <div>No Character</div>;
+    } else {
+      history.push("/");
+    }
+  }
+
   return (
     <div>
+      <button onClick={handleClick}>Back</button>
       <ul>
         <li>
           <img src={image} alt={name} />
         </li>
-
         <li>{name}</li>
-
         <li>{status}</li>
       </ul>
     </div>
